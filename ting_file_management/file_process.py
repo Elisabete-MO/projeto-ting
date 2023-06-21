39cf78a3a1c798104ed89b3f535170b9261d9436
Bete.mrt <bete.mrt@gmail.com>
2023-06-21 18:14:30 -0300

    process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # Verifica se o arquivo já foi processado anteriormente
+    for index in range(len(instance)):
+        if path_file == instance.queue[index]["nome_do_arquivo"]:
+            return None
+
+    lines = txt_importer(path_file)
+
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    # Adiciona o dicionário à fila
+    instance.enqueue(processed_data)
+
+    print(processed_data, file=sys.stdout)
 
 
 def remove(instance):
