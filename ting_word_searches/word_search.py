6ad2639eb86fb1a2de7d3c2abc5789bd906f8bd1
Bete.mrt <bete.mrt@gmail.com>
2023-06-21 22:13:43 -0300

    exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for file_data in instance.queue:
+        file_name = file_data["nome_do_arquivo"]
+        lines_with_word = []
+
+        for line_num, line in enumerate(file_data["linhas_do_arquivo"],
+                                        start=1):
+            if word.lower() in line.lower():
+                lines_with_word.append({"linha": line_num})
+
+        if lines_with_word:
+            file_result = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": lines_with_word
+            }
+            results.append(file_result)
+
+    return results
 
 
 def search_by_word(word, instance):
