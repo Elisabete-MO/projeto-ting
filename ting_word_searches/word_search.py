4775084ceb74d723478942305882e5013bb17400
Bete.mrt <bete.mrt@gmail.com>
2023-06-21 22:28:36 -0300

    search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,27 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for file_data in instance.queue:
+        file_name = file_data["nome_do_arquivo"]
+        lines_with_word = []
+
+        for line_num, line_content in enumerate(file_data["linhas_do_arquivo"],
+                                                start=1):
+            if word.lower() in line_content.lower():
+                line_result = {
+                    "linha": line_num,
+                    "conteudo": line_content.strip()
+                }
+                lines_with_word.append(line_result)
+
+        if lines_with_word:
+            file_result = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": lines_with_word
+            }
+            results.append(file_result)
+
+    return results
